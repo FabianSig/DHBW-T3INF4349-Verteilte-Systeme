4358592a7d911449527aca97c6508860ea0d196c
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-11-29 07:55:39 +0100

    add: frontend random usernames

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {generate} from "random-words";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DHBW-T3INF4349-Verteilte-Systeme-Frontend';
+
+  username;
+
+  constructor() {
+    const randomWords = generate({
+      exactly: 2,
+      wordsPerString: 1,
+      formatter: (word) => word.slice(0, 1).toUpperCase().concat(word.slice(1)),
+    });
+
+    this.username = `${randomWords[0]}_${randomWords[1]}`;
+  }
+
+
 }
