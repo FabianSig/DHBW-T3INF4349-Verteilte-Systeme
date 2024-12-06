228306d2b87caaf35dbf16162d01b3e35024cd88
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-12-06 15:37:35 +0100

    fix: make username persistent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,25 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {generate} from "random-words";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DHBW-T3INF4349-Verteilte-Systeme-Frontend';
+    title = 'DHBW-T3INF4349-Verteilte-Systeme-Frontend';
 
-  username;
+    username: string;
 
-  constructor() {
-    const randomWords = generate({
-      exactly: 2,
-      wordsPerString: 1,
-      formatter: (word) => word.slice(0, 1).toUpperCase().concat(word.slice(1)),
-    });
+    constructor() {
+        this.username = localStorage.getItem("username") ?? this.generateName();
+        localStorage.setItem("username", this.username);
+    }
 
-    this.username = `${randomWords[0]}_${randomWords[1]}`;
-  }
+    private generateName(): string {
+        const randomWords = <string>generate({
+            exactly: 2,
+            wordsPerString: 1,
+            formatter: (word) => word.slice(0, 1).toUpperCase().concat(word.slice(1)),
+        });
 
+        return `${randomWords[0]}_${randomWords[1]}`;
+    }
 
 }
