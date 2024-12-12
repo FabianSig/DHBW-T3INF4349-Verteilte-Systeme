6bfbee7577aa84c49062be5e6cb3255f3b3e3822
fabian <fabian.sigmund1@gmail.com>
2024-12-12 16:41:07 +0100

    fix: createSystemMessage

--- server/src/main/java/fabiansig/factory/MessageFactory.java
+++ server/src/main/java/fabiansig/factory/MessageFactory.java
@@ -3,18 +3,29 @@ package fabiansig.factory;
 import fabiansig.model.Message;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MessageFactory {
 
     public static Message createStrikedMessage(String username, boolean banned) {
 
+        log.debug("Banned has value: {}", banned);
+
         MessageType type = banned ? MessageType.BAN : MessageType.STRIKE;
+
         return createSystemMessage(username, type);
     }
 
     public static Message createSystemMessage(String username, MessageType type) {
 
-        return Message.builder().name("System").content(username + type.getValue()).build();
+        Message systemMessage = new Message();
+        systemMessage.setName("System");
+        systemMessage.setContent(username + type.getValue());
+
+        log.debug("System message: {}", systemMessage);
+
+        return systemMessage;
     }
 
     @Getter
