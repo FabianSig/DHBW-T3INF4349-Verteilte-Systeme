aaeab0a84197ad0d661200b65a36921723603bdb
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-12-13 08:22:26 +0100

    fix: history crossorigin

--- historyService/src/main/java/fabiansig/historyservice/controller/HistoryController.java
+++ historyService/src/main/java/fabiansig/historyservice/controller/HistoryController.java
@@ -7,12 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin
 public class HistoryController {
 
     private final HistoryService historyService;
